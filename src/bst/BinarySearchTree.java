cada2a7e7093356bd41d811bc1f6fa97ef18629f
Karol <pokomeda.karol@gmail.com>
2017-05-10 07:13:55 +0200

    Define BinarySearchTreeInterface

---
+++ src/bst/BinarySearchTree.java
@@ -0,0 +1,18 @@
+package bst;
+
+import java.util.List;
+
+/**
+ * Created by Karol Pokomeda on 2017-05-10.
+ */
+public interface BinarySearchTree<T> {
+    T minValue();
+    T maxValue();
+    T previousValue(T t) throws IndexOutOfBoundsException;
+    T nextValue(T t) throws IndexOutOfBoundsException;
+    boolean add(T t);
+    boolean delete(T t);
+    List<T> getAllInOrder();
+    List<T> getAllPreOrder();
+    List<T> getAllPostOrder();
+}

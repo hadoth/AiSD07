0c67437217dd1b7626f161c2abc7f7adab237fd4
Karol <pokomeda.karol@gmail.com>
2017-05-14 08:38:20 +0200

    Implement RBTree class

---
+++ src/bst/RBTree.java
@@ -0,0 +1,90 @@
+package bst;
+
+import java.util.List;
+
+/**
+ * Created by Karol Pokomeda on 2017-05-14.
+ */
+public class RBTree<T> implements BinarySearchTree<T> {
+
+
+    @Override
+    public T minValue() {
+        return null;
+    }
+
+    @Override
+    public T maxValue() {
+        return null;
+    }
+
+    @Override
+    public T previousValue(T t) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public T nextValue(T t) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(T t) {
+        return false;
+    }
+
+    @Override
+    public List<T> getAllInOrder() {
+        return null;
+    }
+
+    @Override
+    public List<T> getAllPreOrder() {
+        return null;
+    }
+
+    @Override
+    public List<T> getAllPostOrder() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int height() {
+        return 0;
+    }
+
+    @Override
+    public int misbalance() {
+        return 0;
+    }
+
+    @Override
+    public int maxMisbalance() {
+        return 0;
+    }
+
+    @Override
+    public int leavesCount() {
+        return 0;
+    }
+
+    @Override
+    public BinarySearchTree<T> subtree(T t) {
+        return null;
+    }
+}
